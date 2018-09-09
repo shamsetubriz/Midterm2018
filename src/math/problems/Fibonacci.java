15ec4b2a07dd9e57e735b3c7d35b7f1f872758f6
shamsetubriz <qjshams@gmail.com>
2018-09-09 18:36:18 -0400

    finding lowest number math problem added

--- src/math/problems/Fibonacci.java
+++ src/math/problems/Fibonacci.java
@@ -7,7 +7,7 @@ public class Fibonacci {
          */
         int next = 0;
         int prev = 1;
-        for(int i=0; i<10; i++){
+        for(int i=0; i<40; i++){
             System.out.println(next);
             next = next + prev;
             prev = next - prev;
--- src/math/problems/FindLowestDifference.java
+++ src/math/problems/FindLowestDifference.java
@@ -1,14 +1,58 @@
 package math.problems;
 
+import java.util.Arrays;
+
 public class FindLowestDifference {
+
+    //Funtion to calculate small result between two Arrays.
+    static int findSmallestDifference(int A[], int B[], int m, int n)
+
+    {
+        //Sort both arrays using sort function
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int a = 0, b = 0;
+
+        //initialize result as max value
+        int result = Integer.MAX_VALUE;
+
+        //scan both Arrays upto size of the Arrays
+        while (a < m && b < n)
+        {
+            if (Math.abs(A[a] - B[b]) < result)
+                result = Math.abs(A[a] - B[b]);
+
+            //move smaller value
+            if (A[a] < B[b])
+                a++;
+            else
+                b++;
+        }
+
+        //return final result
+        return result;
+    }
+
+    //Driver Code
     public static void main(String[] args) {
         /*
          Implement in java.
          Read this below two array. Find the lowest difference between the two array cell.
         */
-        int [] array1 = {30,12,5,9,2,20,33,1};
-        int [] array2 = {18,25,41,47,17,36,14,19};
 
+        //Input given array A
+        int A[] = {30,12,5,9,2,20,33,1};
+
+        //Input given array B
+        int B[] = {18,25,41,47,17,36,14,19};
+
+        //Calculate size of both arrays
+        int m = A.length;
+        int n = B.length;
+
+        //Call function to print smallest result
+        System.out.println(findSmallestDifference(A, B, m, n));
 
 
 
