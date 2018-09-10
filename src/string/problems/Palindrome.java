5574ea3ad7973cbe1b3fb466a52de8b27d157b01
shamsetubriz <qjshams@gmail.com>
2018-09-09 22:07:10 -0400

    Palindrome problem is solved

--- src/string/problems/Palindrome.java
+++ src/string/problems/Palindrome.java
@@ -1,11 +1,29 @@
 package string.problems;
 
-public class Palindrome {
-    public static void main(String[] args) {
-        /*
+import java.sql.SQLOutput;
+ /*
           If a String is reversed and it remains unchanged, that is called Palindrome. For example, MOM,DAD,MADAM are
           Palindrome. So write java code to check if a given String is Palindrome or not.
          */
+
+
+public class Palindrome {
+
+    public static void checkpalindrome(String s)
+    {
+        //following code will reverse the given String.
+        String reverse = new StringBuffer(s).reverse().toString();
+
+        //Following command will check whether the String is palindrome or not.
+        if (s.equals(reverse))
+            System.out.println("Yes");
+        else
+                System.out.println("No");
+}
+    public static void main(String[] args) throws java.lang.Exception
+    {
+        checkpalindrome("repper");
+        checkpalindrome("peopleNtech");
 
     }
 }
