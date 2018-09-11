a94eb594d074857e1e798a4f71a82ce0c2b7ef1c
shamsetubriz <qjshams@gmail.com>
2018-09-11 02:33:06 -0400

    UseQueue

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,6 +1,9 @@
 package datastructure;
 
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class UseQueue {
 
@@ -10,8 +13,37 @@ public class UseQueue {
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 *
 		 */
-	}
+		Queue<Integer> newQ = new LinkedList<Integer>();
+
+		// Add elements.
+		newQ.add(212);
+		newQ.add(84);
+		newQ.add(473);
+		newQ.add(5642);
+		newQ.add(233);
+
+		//peek, poll, remove
+		System.out.println(newQ.peek());
+		System.out.println(newQ.poll());
+		System.out.println(newQ.peek());
+		System.out.println(newQ.remove());
+		System.out.println(newQ.peek());
+
+		//For Each loop.
+
+		System.out.println("Use of for each loop to retrieve data: ");
+		for (Integer in : newQ) {
+			System.out.println(in);
+		}
+
+		//while loop with Iterator.
+		System.out.println("Use of while loop to retrieve data: ");
+		Iterator itr = newQ.iterator();
+		while (itr.hasNext()) {
+			Integer i = (Integer) itr.next();
+			System.out.println(i);
+		}
 
 	}
-
+}
 
