3ff5a3421b28a96e4ab929473aa5800bf211762f
shamsetubriz <qjshams@gmail.com>
2018-09-10 23:09:24 -0400

    Data reader subclass is partially done

--- src/datastructure/DataReader.java
+++ src/datastructure/DataReader.java
@@ -1,11 +1,15 @@
 package datastructure;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class DataReader {
 
 	public static void main(String[] args) {
 		/*
 		 * User API to read the below textFile and print to console.
-		 * Use BufferedReader class. 
+		 * Use BufferedReader class.
 		 * Use try....catch block to handle Exception.
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
@@ -18,7 +22,26 @@ public class DataReader {
 		 * Use For Each loop/while loop/Iterator to retrieve data.
 		 */
 
-		String textFile = System.getProperty("user.dir") + "/src/data/self-driving-car.txt";
+		String textFile = "/Users/shams/Applications/Develop/MidtermJuly2018/src/data/self-driving-car";
+		FileReader fr = null;
+		BufferedReader br = null;
+
+		try{
+			fr = new FileReader(textFile);
+			System.out.println("The file has been found");
+		}catch (Exception e){
+			System.out.println("The file has not been found");
+
+		}
+		try {
+			br = new BufferedReader(fr);
+			String data = "";
+			while ((data = br.readLine()) != null) {
+				System.out.println(data);
+			}
+		}catch (Exception ex){
+			System.out.println("The data you are looking for is not available. ");
+		}
 
 
 
