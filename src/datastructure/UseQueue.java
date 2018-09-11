c1d1e368d3d9695ee5ce5b2c9e01931b2ebfa3c6
shamsetubriz <qjshams@gmail.com>
2018-09-10 21:37:06 -0400

    DuplicateWord problem problem added

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.Arrays;
+
 public class UseQueue {
 
 	public static void main(String[] args) {
@@ -8,7 +10,29 @@ public class UseQueue {
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 * 
 		 */
+		String st1 = "BRAG";
+		String st2 = "GRAB";
+		String st3 = "NIGHT";
+		String st4 = "THING";
+		System.out.println(isAnagram(st1, st2));
+		System.out.println(isAnagram(st3, st4));
+		System.out.println(isAnagram(st1, st4));
+		System.out.println(isAnagram(st2, st3));
 
 	}
 
-}
+	public static boolean isAnagram(String st1, String st2) {
+		char[] firstWord = st1.toCharArray();
+		char[] secondWord = st2.toCharArray();
+
+		Arrays.sort(firstWord);
+		Arrays.sort(secondWord);
+
+
+		return Arrays.equals(firstWord, secondWord);
+	}
+
+
+	}
+
+
--- src/string/problems/DuplicateWord.java
+++ src/string/problems/DuplicateWord.java
@@ -1,5 +1,8 @@
 package string.problems;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by mrahman on 04/22/17.
  */
@@ -11,6 +14,35 @@ public class DuplicateWord {
          * Also Find the average length of the words.
          */
         String st = "Java is a programming Language. Java is also an Island of Indonesia. Java is widely used language";
+
+        String[] words = st.split(" ");
+        Map<String, Integer> wordMap = new HashMap<String, Integer>();
+        Integer wordCount = 0;
+        for (int i = 0; i < words.length; i++){
+            String word = words[i].toUpperCase();
+
+            if (wordMap.get(word) == null){
+                wordMap.put(word, wordCount);
+            }else{
+                System.out.println("Duplicate word: " +word);
+
+                wordMap.replace(word, wordMap.get(word) + 1);
+                System.out.println("Number of occurrence: " + wordMap.get(word));
+            }
+
+        }
+        int sum = 0, count = 0;
+                for (String str : wordMap.keySet()){
+
+                    if(wordMap.get(str) > 0){
+                        sum += str.length();
+                        count++;
+                    }
+                }
+
+        System.out.println("Average length of owrd is: " + sum/count);
+
+
 
     }
 
