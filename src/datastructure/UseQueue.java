3841e432c92351fc14e72808afe408c98337f1f6
shamsetubriz <qjshams@gmail.com>
2018-09-10 21:56:15 -0400

    Anagram problem modified

--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -8,30 +8,9 @@ public class UseQueue {
 		/*
 		 * Demonstrate how to use Queue that includes add,peek,remove,pool elements.
 		 * Use For Each loop and while loop with Iterator to retrieve data.
-		 * 
+		 *
 		 */
-		String st1 = "BRAG";
-		String st2 = "GRAB";
-		String st3 = "NIGHT";
-		String st4 = "THING";
-		System.out.println(isAnagram(st1, st2));
-		System.out.println(isAnagram(st3, st4));
-		System.out.println(isAnagram(st1, st4));
-		System.out.println(isAnagram(st2, st3));
-
 	}
-
-	public static boolean isAnagram(String st1, String st2) {
-		char[] firstWord = st1.toCharArray();
-		char[] secondWord = st2.toCharArray();
-
-		Arrays.sort(firstWord);
-		Arrays.sort(secondWord);
-
-
-		return Arrays.equals(firstWord, secondWord);
-	}
-
 
 	}
 
--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -1,5 +1,7 @@
 package string.problems;
 
+import java.util.Arrays;
+
 /**
  * Created by mrahman on 04/22/17.
  */
@@ -11,9 +13,30 @@ public class Anagram {
 
     public static void main(String[] args)
     {
+        String st1 = "BRAG";
+        String st2 = "GRAB";
+        String st3 = "NIGHT";
+        String st4 = "THING";
+        System.out.println(isAnagram(st1, st2));
+        System.out.println(isAnagram(st3, st4));
+        System.out.println(isAnagram(st1, st4));
+        System.out.println(isAnagram(st2, st3));
+
+    }
+
+    public static boolean isAnagram(String st1, String st2) {
+        char[] firstWord = st1.toCharArray();
+        char[] secondWord = st2.toCharArray();
+
+        Arrays.sort(firstWord);
+        Arrays.sort(secondWord);
+
+
+        return Arrays.equals(firstWord, secondWord);
+    }
 
 
 
 
     }
-}
+
